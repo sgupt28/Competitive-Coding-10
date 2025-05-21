2dbdc5123d803287dbd9507ecf9bb83d2963c6b5
sgupt28 <shubhangiasu28@gmail.com>
2025-05-21 02:22:47 -0400

    Completed Problem 1

---
+++ BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,20 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :yes
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int maxProfit=0;
+
+        for(int i=1;i<prices.length;i++){
+
+            if(prices[i]>prices[i-1]){
+                maxProfit=maxProfit+(prices[i]-prices[i-1]);
+            }
+        }
+        return maxProfit;
+    }
+}
+
